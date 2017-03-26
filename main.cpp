a28943d3bcc1e3dff3fdd63239ee6134957d8671
vnetware <yangboduan@sina.com>
2017-03-26 10:27:36 +0800

    capture package

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <netinet/if_ether.h>
 #include <linux/ip.h>
 #include <netinet/tcp.h>
+#include <netinet/udp.h>
 #include <iostream>
 #include <ctime>
 using namespace std;
@@ -16,13 +17,16 @@ using namespace std;
 void getPacket(u_char * arg, const struct pcap_pkthdr * pkthdr, const u_char * packet)  
 { 
     struct in_addr addr;
-    struct tcphdr *tcpptr; 
+    struct ether_header *ethernet_hdrptr; //以太网头部 
+    struct iphdr *ipptr; 
+    struct tcphdr *tcphdrptr;//TCP头部结构体 
+    struct udphdr *udphdrptr;//UDP头部结构体
+    struct ether_arp *arp;
+ 
     int * id = (int *)arg;  
     unsigned char *mac_string;                
-    struct ether_header *ethernet_protocol; //以太网头部 
     unsigned short ethernet_type;           //二层头部的以太网类型 
-    struct iphdr *ipptr; 
-    ethernet_protocol = (struct ether_header *)packet;
+    ethernet_hdrptr = (struct ether_header *)packet;
    
     //printf("id: %d\n", ++(*id));  //抓包计数
     //printf("Packet length: %d\n", pkthdr->len);  
@@ -31,12 +35,12 @@ void getPacket(u_char * arg, const struct pcap_pkthdr * pkthdr, const u_char * p
   
        //分析二层头部信息 
     cout <<"二层头部解析:\t["; 
-    mac_string = (unsigned char *)ethernet_protocol->ether_shost;//获取源mac地址  
+    mac_string = (unsigned char *)ethernet_hdrptr->ether_shost;//获取源mac地址  
         printf("源MAC地址: %02x:%02x:%02x:%02x:%02x:%02x  ",*(mac_string+0),*(mac_string+1),*(mac_string+2),*(mac_string+3),*(mac_string+4),*(mac_string+5)); //输出源MAC地址
 
-        mac_string = (unsigned char *)ethernet_protocol->ether_dhost;//获取目的mac  
+        mac_string = (unsigned char *)ethernet_hdrptr->ether_dhost;//获取目的mac  
         printf("目的MAC地址: %02x:%02x:%02x:%02x:%02x:%02x  ",*(mac_string+0),*(mac_string+1),*(mac_string+2),*(mac_string+3),*(mac_string+4),*(mac_string+5)); //输出目的MAC地址  
-    ethernet_type = ntohs(ethernet_protocol->ether_type);//获得以太网的类型
+    ethernet_type = ntohs(ethernet_hdrptr->ether_type);//获得以太网的类型
   
     //分析数据包二层头部，解析出上层协议
     if (ethernet_type == ETHERTYPE_IP){ //IP protocol
@@ -61,14 +65,20 @@ void getPacket(u_char * arg, const struct pcap_pkthdr * pkthdr, const u_char * p
 		cout <<"TCP ]"<<endl;
 		//分析四层头部信息
 		cout <<"四层头部解析:\t"<<"["; 
-		tcpptr = (struct tcphdr*)(packet+sizeof(struct ether_header)+sizeof(struct iphdr));//得到tcp包头
-		cout <<"源端口:"<<ntohs(tcpptr->source)<<"  目的端口:"<<ntohs(tcpptr->dest);
+		tcphdrptr = (struct tcphdr*)(packet+sizeof(struct ether_header)+sizeof(struct iphdr));//得到tcp包头
+		cout <<"源端口:"<<ntohs(tcphdrptr->source)<<"  目的端口:"<<ntohs(tcphdrptr->dest);
 		cout<<" ]";
 		break;
 	    }
 	    case 17://UDP
 	    {
 		cout <<"UDP ]"<<endl;
+		//分析四层头部信息
+                cout <<"四层头部解析:\t"<<"[";
+                udphdrptr = (struct udphdr*)(packet+sizeof(struct ether_header)+sizeof(struct iphdr));//得到tcp包头
+                cout <<"源端口:"<<ntohs(udphdrptr->source)<<"  目的端口:"<<ntohs(udphdrptr->dest);
+                cout<<" ]";
+
 		break;
 	    }
 	} 
@@ -76,10 +86,10 @@ void getPacket(u_char * arg, const struct pcap_pkthdr * pkthdr, const u_char * p
     else if (ethernet_type == ETHERTYPE_ARP){//ARP protocol
   	cout <<"Type:ARP "<<endl;
 	//解析出源目MAC地址 
-	mac_string = (unsigned char *)ethernet_protocol->ether_shost;//获取源mac地址  
+	mac_string = (unsigned char *)ethernet_hdrptr->ether_shost;//获取源mac地址  
 	printf("源MAC地址  : %02x:%02x:%02x:%02x:%02x:%02x  ",*(mac_string+0),*(mac_string+1),*(mac_string+2),*(mac_string+3),*(mac_string+4),*(mac_string+5)); //输出源MAC地址
   
-	mac_string = (unsigned char *)ethernet_protocol->ether_dhost;//获取目的mac  
+	mac_string = (unsigned char *)ethernet_hdrptr->ether_dhost;//获取目的mac  
 	printf("目的MAC地址: %02x:%02x:%02x:%02x:%02x:%02x\n",*(mac_string+0),*(mac_string+1),*(mac_string+2),*(mac_string+3),*(mac_string+4),*(mac_string+5)); //输出目的MAC地址 
     }
     else if (ethernet_type == 0x0835)//ARP protocol
@@ -101,43 +111,40 @@ void getPacket(u_char * arg, const struct pcap_pkthdr * pkthdr, const u_char * p
 
 }  
   
-int main()  
-{  
-  char errBuf[PCAP_ERRBUF_SIZE], * devStr;  
-    
-  /* get a device */  
-  devStr = pcap_lookupdev(errBuf);  
-    
-  if(devStr)  
-  { 
-    cout<<"Listen on interface "<<devStr<<endl; 
-  }  
-  else  
-  {  
-    printf("error: %s\n", errBuf);  
-    exit(1);  
-  }  
-    
-  /* open a device, wait until a packet arrives */  
-  pcap_t * device = pcap_open_live(devStr, 65535, 1, 0, errBuf);  
-    
-  if(!device)  
-  {  
-    printf("error: pcap_open_live(): %s\n", errBuf);  
-    exit(1);  
-  }  
-    
-  /* construct a filter */  
-  struct bpf_program filter;  
-//  pcap_compile(device, &filter, "dst port 23", 1, 0);  
-
- // pcap_setfilter(device, &filter);  
-    
-  /* wait loop forever */  
-  int id = 0;  
-  pcap_loop(device, -1, getPacket, (u_char*)&id);  
-    
-  pcap_close(device);  
+int main()  {  
+    char errBuf[PCAP_ERRBUF_SIZE], * interfaceName;  
+      
+    interfaceName = pcap_lookupdev(errBuf); //获取网络接口设备名,如eth0 
+      
+    if(interfaceName)  
+    { 
+      cout<<"Listen on interface "<<interfaceName<<endl; 
+    }  
+    else  
+    {  
+      printf("error: %s\n", errBuf);  
+      exit(1);  
+    }  
+      
+    pcap_t * device = pcap_open_live(interfaceName, 65535, 1, 0, errBuf); //打开一个用于捕获数据的网络接口 
+      
+    if(!device)  
+    {  
+      printf("error: pcap_open_live(): %s\n", errBuf);  
+      exit(1);  
+    }  
+      
+    //struct bpf_program filter; //bpf_program结 构的指针,用于pcap_compile，格式过滤
+    //pcap_compile(device, &filter, "dst port 23", 1, 0); //编译 BPF 过滤规则 
   
-  return 0;  
+    //pcap_setfilter(device, &filter); //应用 BPF 过滤规则 
+      
+    int id = 0; 
+  
+    //循环捕获网络数据包，直到遇到错误或者满足退出条件。每次捕获一个数据包就会调用 callback 指定的回调函数(此处为getPacket)
+    pcap_loop(device, -1, getPacket, (u_char*)&id);  
+      
+    pcap_close(device); //释放网络接口 
+    
+    return 0;  
 }  

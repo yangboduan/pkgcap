cc86c3773e2dc1139c1303c720bc97c47dbfe010
vnetware <yangboduan@sina.com>
2017-03-26 11:00:48 +0800

    capture package

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ void getPacket(u_char * arg, const struct pcap_pkthdr * pkthdr, const u_char * p
 { 
     struct in_addr addr;
     struct ether_header *ethernet_hdrptr; //以太网头部 
-    struct iphdr *ipptr; 
+    struct iphdr *iphdrptr;  //IP头部结构体 
     struct tcphdr *tcphdrptr;//TCP头部结构体 
     struct udphdr *udphdrptr;//UDP头部结构体
     struct ether_arp *arp;
@@ -33,28 +33,28 @@ void getPacket(u_char * arg, const struct pcap_pkthdr * pkthdr, const u_char * p
     //printf("Number of bytes: %d\n", pkthdr->caplen);
     printf("抓包时间: %s", ctime((const time_t *)&pkthdr->ts.tv_sec));   
   
-       //分析二层头部信息 
-    cout <<"二层头部解析:\t["; 
-    mac_string = (unsigned char *)ethernet_hdrptr->ether_shost;//获取源mac地址  
-        printf("源MAC地址: %02x:%02x:%02x:%02x:%02x:%02x  ",*(mac_string+0),*(mac_string+1),*(mac_string+2),*(mac_string+3),*(mac_string+4),*(mac_string+5)); //输出源MAC地址
+    //分析二层头部信息 
+    cout <<"二层头部解析: ["; 
+    mac_string = (unsigned char *)ethernet_hdrptr->ether_shost;//获取源mac地址 
+    printf("源MAC地址: %02x:%02x:%02x:%02x:%02x:%02x  ",*(mac_string+0),*(mac_string+1),*(mac_string+2),*(mac_string+3),*(mac_string+4),*(mac_string+5)); //输出源MAC地址
 
-        mac_string = (unsigned char *)ethernet_hdrptr->ether_dhost;//获取目的mac  
-        printf("目的MAC地址: %02x:%02x:%02x:%02x:%02x:%02x  ",*(mac_string+0),*(mac_string+1),*(mac_string+2),*(mac_string+3),*(mac_string+4),*(mac_string+5)); //输出目的MAC地址  
+    mac_string = (unsigned char *)ethernet_hdrptr->ether_dhost;//获取目的mac  
+    printf("目的MAC地址: %02x:%02x:%02x:%02x:%02x:%02x  ",*(mac_string+0),*(mac_string+1),*(mac_string+2),*(mac_string+3),*(mac_string+4),*(mac_string+5)); //输出目的MAC地址  
     ethernet_type = ntohs(ethernet_hdrptr->ether_type);//获得以太网的类型
   
     //分析数据包二层头部，解析出上层协议
     if (ethernet_type == ETHERTYPE_IP){ //IP protocol
 	cout<<"Type:IP ]"<<endl; 
 	 
-	cout <<"三层头部解析:\t"<<"["; 
-        //分析三层头部信息 
-	addr.s_addr = ipptr->saddr;//目的IP地址
+	cout <<"三层头部解析: ["; 
+        //分析三层头部信息
+	iphdrptr = (struct iphdr*)    (packet+sizeof(struct ether_header));//得到ip包头 
+	addr.s_addr = iphdrptr->saddr;//目的IP地址
 	cout<<"源IP地址:"<< inet_ntoa(addr) <<"  "; 
-	addr.s_addr = ipptr->daddr;//目的IP地址
+	addr.s_addr = iphdrptr->daddr;//目的IP地址
 	cout<<"目的IP地址:"<< inet_ntoa(addr)<<"  ";  
-	ipptr = (struct iphdr*)    (packet+sizeof(struct ether_header));//得到ip包头 
 	cout <<"Protocol:"; 
-	switch(ipptr->protocol){//三层IP报文头部IP协议类型解析
+	switch(iphdrptr->protocol){//三层IP报文头部IP协议类型解析
 	    case 1://ICMP
 	    {
 		cout <<"ICMP ]"<<endl;
@@ -64,7 +64,7 @@ void getPacket(u_char * arg, const struct pcap_pkthdr * pkthdr, const u_char * p
 	    {
 		cout <<"TCP ]"<<endl;
 		//分析四层头部信息
-		cout <<"四层头部解析:\t"<<"["; 
+		cout <<"四层头部解析: ["; 
 		tcphdrptr = (struct tcphdr*)(packet+sizeof(struct ether_header)+sizeof(struct iphdr));//得到tcp包头
 		cout <<"源端口:"<<ntohs(tcphdrptr->source)<<"  目的端口:"<<ntohs(tcphdrptr->dest);
 		cout<<" ]";
@@ -133,10 +133,10 @@ int main()  {
       printf("error: pcap_open_live(): %s\n", errBuf);  
       exit(1);  
     }  
-      
+    
+    /*  以下三行的注释去掉，则开启包过滤功能*/  
     //struct bpf_program filter; //bpf_program结 构的指针,用于pcap_compile，格式过滤
     //pcap_compile(device, &filter, "dst port 23", 1, 0); //编译 BPF 过滤规则 
-  
     //pcap_setfilter(device, &filter); //应用 BPF 过滤规则 
       
     int id = 0; 
